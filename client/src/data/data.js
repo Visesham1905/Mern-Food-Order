c66db1fdbebef438a77b7927728c2a1c0c3d8131
shyam vise <shyamvise2016@gmail.com>
2023-11-30 13:09:50 +0530

    first commit

---
+++ client/src/data/data.js
@@ -0,0 +1,192 @@
+import burger1 from '../assets/burger1.jpg'
+import burger2 from '../assets/burger2.jpg'
+import burger3 from '../assets/burger3.jpg'
+import burger4 from '../assets/burger4.jpg'
+import burger5 from '../assets/burger5.jpg'
+
+import gyros1 from '../assets/gyros1.jpg'
+import gyros2 from '../assets/gyros2.jpg'
+import gyros3 from '../assets/gyros3.jpg'
+import gyros4 from '../assets/gyros4.jpg'
+import gyros5 from '../assets/gyros5.jpg'
+
+import pizza1 from '../assets/pizza1.jpg'
+import pizza2 from '../assets/pizza2.jpg'
+import pizza3 from '../assets/pizza3.jpg'
+import pizza4 from '../assets/pizza4.jpg'
+import pizza5 from '../assets/pizza5.jpg'
+
+import pasta1 from '../assets/pasta1.jpg'
+import pasta2 from '../assets/pasta2.jpg'
+import pasta3 from '../assets/pasta3.jpg'
+import pasta4 from '../assets/pasta4.jpg'
+import pasta5 from '../assets/pasta5.jpg'
+
+
+export const foodTypes = [
+    {
+        name: 'burger',
+        img: burger1,
+        id: crypto.randomUUID()
+    },
+    {
+        name: 'gyros',
+        img: gyros1,
+        id: crypto.randomUUID()
+    },
+    {
+        name: 'pizza',
+        img: pizza1,
+        id: crypto.randomUUID()
+    },
+    {
+        name: 'pasta',
+        img: pasta1,
+        id: crypto.randomUUID()
+    },
+]
+
+
+// 24 foods (6 burger, 6 gyros, 6 pizza, 6 pasta)
+export const foods = [
+    {
+        name: 'burger1',
+        category: 'burger',
+        img: burger1,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'burger2',
+        category: 'burger',
+        img: burger2,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'burger3',
+        category: 'burger',
+        img: burger3,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'burger4',
+        category: 'burger',
+        img: burger4,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'burger5',
+        category: 'burger',
+        img: burger5,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pizza1',
+        category: 'pizza',
+        img: pizza1,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pizza2',
+        category: 'pizza',
+        img: pizza2,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pizza3',
+        category: 'pizza',
+        img: pizza3,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pizza4',
+        category: 'pizza',
+        img: pizza4,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pizza5',
+        category: 'pizza',
+        img: pizza5,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'gyros1',
+        category: 'gyros',
+        img: gyros1,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'gyros2',
+        category: 'gyros',
+        img: gyros2,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'gyros3',
+        category: 'gyros',
+        img: gyros3,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'gyros4',
+        category: 'gyros',
+        img: gyros4,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'gyros5',
+        category: 'gyros',
+        img: gyros5,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pasta1',
+        category: 'pasta',
+        img: pasta1,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pasta2',
+        category: 'pasta',
+        img: pasta2,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pasta3',
+        category: 'pasta',
+        img: pasta3,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pasta4',
+        category: 'pasta',
+        img: pasta4,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+    {
+        name: 'pasta5',
+        category: 'pasta',
+        img: pasta5,
+        id: crypto.randomUUID(),
+        price: Math.floor(Math.random() * 20 + 7)
+    },
+]
